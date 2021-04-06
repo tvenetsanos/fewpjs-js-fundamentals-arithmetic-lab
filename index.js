24b545c4aaf0332480ef33012504564e91b91ca2
Tyler <tyler_venetsanos@msn.com>
2021-04-06 16:19:34 -0400

    commit tree 7589d737a2031e125939e5cd25366db5ac8be716

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 2) + 1;
+
+const num3 = 19
+const num4 = 5
+let mod = num3 % num4;
+
+let max = Math.max(2, 4, 8, 16 , 20);
